91a381121f99cf9567633a5c9e4cfe21344ea0a8
phil65 <phil65@kodi.tv>
2019-04-04 06:31:25 +0200

    application.py work

--- prettyqt/widgets/applicaton.py
+++ prettyqt/widgets/applicaton.py
@@ -15,7 +15,23 @@ class Application(QtWidgets.QApplication):
                 icon = qta.icon(icon, color="lightgray")
             self.setWindowIcon(icon)
 
-    def set_language(self, path):
+    def load_language_file(self, path):
         translator = QtCore.QTranslator(self)
-        translator.load(path)
+        translator.load(str(path))
         self.installTranslator(translator)
+
+    def set_metadata(self, app_name=None, org_name=None, org_domain=None):
+        if app_name:
+            self.setApplicationName(app_name)
+        if org_name:
+            self.setOrganizationName(org_name)
+        if org_domain:
+            self.setOrganizationDomain(org_domain)
+
+    @classmethod
+    def use_hdpi_bitmaps(cls):
+        QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps)
+
+    @classmethod
+    def disable_window_help_button(cls):
+        QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_DisableWindowContextHelpButton)
